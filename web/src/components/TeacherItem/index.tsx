e4ebfc6d3fae2e2ef678b41c18c5980dc1748352
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-08-05 20:23:21 -0300

    Saerch for classes

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -5,30 +5,43 @@ import iconWhatsapp from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.scss';
 
-export default function TeacherItem() {
+interface TeacherItemProps {
+  classInfo: {
+    "user_id": Number;
+    "name": string;
+    "avatar": string;
+    "title": string;
+    "bio": string;
+    "subject": string;
+    "cost": Number;
+    "whatsapp": string;
+  };
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = props => {
   return (
     <article className="teacher-item">
       <header>
         <img src="https://avatars2.githubusercontent.com/u/5047991?s=460&u=78793349fb74e7bb9178013f388337a05c4085f3&v=4" alt="Pedro Sanção"/>
         <div>
-          <h3>Pedro Sanção</h3>
-          <span>Matemática</span>
+          <h3>{props.classInfo.name}</h3>
+          <span>{props.classInfo.subject}</span>
         </div>
       </header>
       <p>
-        <strong>Sed velit quam, lacinia non metus sit amet, venenatis bibendum eros. Fusce pharetra interdum luctus. </strong>
+        <strong>{props.classInfo.title}</strong>
       </p>
-      <p>In a condimentum sem. Fusce iaculis sollicitudin iaculis. Maecenas vitae velit sed nisl facilisis euismod ornare non sapien. Praesent venenatis elementum pretium. Aenean tincidunt, lectus in ultrices gravida, justo nisi commodo metus, in venenatis ipsum nisi nec libero. Cras eu vulputate dolor, ac egestas leo. In consectetur lobortis eros, et finibus nunc pulvinar ut.</p>
+      <p>{props.classInfo.bio}</p>
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 80,00</strong>
+          <strong>R$ {props.classInfo.cost}</strong>
         </p>
         <div className="buttons-container">
           <button id="favorite-button">
             <img src={iconPurpleHeart} alt="Coração roxo"/>
           </button> 
-          <button id="contact-button">
+          <button id="contact-button" data-user-id={props.classInfo.user_id}>
             <img src={iconWhatsapp} alt="Whatsapp"/>
             Entrar em contato
           </button>
@@ -37,3 +50,5 @@ export default function TeacherItem() {
     </article>
   )
 }
+
+export default TeacherItem;
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,27 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from '../../services/api';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
-import SelectSubject from '../../components/SelectSubject';
-import SelectWeekday from '../../components/SelectWeekday';
+import Select from '../../components/Select';
+// import SelectSubject from '../../components/SelectSubject';
+// import SelectWeekday from '../../components/SelectWeekday';
 
 import './styles.scss';
 
 export default function TeacherList() {
+  const [searched, setSearched] = useState(false);
+  const [classes, setClasses]   = useState([]);
+  const [subject, setSubject]   = useState('');
+  const [weekday, setWeekday]   = useState('');
+  const [time, setTime]         = useState('');
+
+  async function searchClasses() {
+    if (!subject || !weekday || !time) {
+      alert('Selecione todos os filtros para pesquisar.');
+      return;
+    }
+
+    setClasses([]);
+    setSearched(false);
+    api.get('classes', { params: {
+      subject,
+      weekday,
+      time
+    } }).then(({ data }) => {
+      setClasses(data);
+    }, error => {
+      console.warn(error);
+      alert('Ocorreu um erro ao buscar, tente novamente.');
+    }).then(() => {
+      setSearched(true);
+    });
+  }
+
   return (
     <div id="page-teacher-list" className="container">
       <PageHeader title="Estes são os proffys disponíveis.">
-        <form id="search-teachers">
-          <SelectSubject/>
-          <SelectWeekday/>
-          <Input name="time" label="Hora" type="time"/>
-        </form>
+        <div id="search-teachers">
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes' },
+              { value: 'Biologia' },
+              { value: 'Desenho Geométrico' },
+              { value: 'Educação Física' },
+              { value: 'Espanhol' },
+              { value: 'Filosofia' },
+              { value: 'Física' },
+              { value: 'Geografia' },
+              { value: 'História' },
+              { value: 'Inglês' },
+              { value: 'Língua Portuguesa e Literatura' },
+              { value: 'Matemática' },
+              { value: 'Química' },
+              { value: 'Sociologia' }
+            ]}
+            value={subject}
+            onChange={ event => { setSubject(event.target.value) } }
+          />
+          <Select
+            name="weekday"
+            label="Dia da semana"
+            options={[
+              { value: '0', label: 'Domingo' },
+              { value: '1', label: 'Segunda-feira' },
+              { value: '2', label: 'Terça-feira' },
+              { value: '3', label: 'Quarta-feira' },
+              { value: '4', label: 'Quinta-feira' },
+              { value: '5', label: 'Sexta-feira' },
+              { value: '6', label: 'Sábado' }
+            ]}
+            value={weekday}
+            onChange={ event => { setWeekday(event.target.value) } }
+          />
+          <Input
+            name="time"
+            label="Hora"
+            type="time"
+            value={time}
+            onChange={ event => { setTime(event.target.value) } }
+          />
+          <button onClick={searchClasses}>Buscar</button>
+        </div>
       </PageHeader>
       <main className="container">
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
-        <TeacherItem/>
+        {classes.map((classItem, index) => {
+          return (
+            <TeacherItem key={index} classInfo={classItem}/>
+          );
+        })}
+        {searched && classes.length === 0 && (
+          <div className="no-results">
+            <p>Nenhum professor encontrado<br/>para sua pesquisa.</p>
+          </div>
+        )}
       </main>
     </div>
   )
