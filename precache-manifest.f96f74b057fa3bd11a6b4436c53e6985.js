294d89555e679e7e0858db189d2031a2cd280938
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-08-06 16:37:26 -0300

    Export pages with static files simulating API

---
+++ precache-manifest.f96f74b057fa3bd11a6b4436c53e6985.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d2b5301e3da33118745d677b6e0de8",
+    "url": "/nlw-proffy/index.html"
+  },
+  {
+    "revision": "004e10e51ac6a1bbbcbf",
+    "url": "/nlw-proffy/static/css/main.a55cd383.chunk.css"
+  },
+  {
+    "revision": "58e965729e8131861c4f",
+    "url": "/nlw-proffy/static/js/2.79997502.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nlw-proffy/static/js/2.79997502.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "004e10e51ac6a1bbbcbf",
+    "url": "/nlw-proffy/static/js/main.9adb7233.chunk.js"
+  },
+  {
+    "revision": "79da39a474cb6fa19b32",
+    "url": "/nlw-proffy/static/js/runtime-main.71782efc.js"
+  },
+  {
+    "revision": "dc8c47fa945838a208074157d997436d",
+    "url": "/nlw-proffy/static/media/back.dc8c47fa.svg"
+  },
+  {
+    "revision": "211f2bb00860f91804cfb2354e17b255",
+    "url": "/nlw-proffy/static/media/landing.211f2bb0.svg"
+  },
+  {
+    "revision": "86fef799b0726c66fb7a9d710e2d922c",
+    "url": "/nlw-proffy/static/media/logo.86fef799.svg"
+  },
+  {
+    "revision": "bfdb3ebe513b5b2411a5014d6aad3abe",
+    "url": "/nlw-proffy/static/media/purple-heart.bfdb3ebe.svg"
+  },
+  {
+    "revision": "e7a7e1b4e016245fa97e6f42aee4bcaa",
+    "url": "/nlw-proffy/static/media/study.e7a7e1b4.svg"
+  },
+  {
+    "revision": "156102ee75a899123483211bbf4a8fbd",
+    "url": "/nlw-proffy/static/media/teach.156102ee.svg"
+  },
+  {
+    "revision": "6dfaa2f94fc54e53666a511b779f90b4",
+    "url": "/nlw-proffy/static/media/warning.6dfaa2f9.svg"
+  },
+  {
+    "revision": "45c6e6ecebde5ee441644d41cc132ce4",
+    "url": "/nlw-proffy/static/media/whatsapp.45c6e6ec.svg"
+  }
+]);
