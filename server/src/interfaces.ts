99d717d63ac07cde606b792888850554fdb5b9fc
Pedro Sanção <pedrosancao@users.noreply.github.com>
2020-08-08 11:52:24 -0300

    Convert server to submodule

--- server/src/interfaces.ts
+++
@@ -1,5 +0,0 @@
-export interface ScheduleItem {
-  weekday: number;
-  timeFrom: string;
-  timeTo: string;
-};
--- server/src/utils.ts
+++
@@ -1,4 +0,0 @@
-export function convertHourToMinutes(time: string) : number {
-  const [hours, minutes] = time.split(':').map(Number);
-  return hours * 60 + minutes;
-}
